a2019b430a8cb0a8a57dd76cfe2ec17ab9f4a03f
Sangn2911 <sangn2911@gmail.com>
2021-06-02 12:00:09 +0700

    add para redeclared, no entry point

--- assignment3-ver1.0/assignment3/initial/src/test/CheckSuite.py
+++ assignment3-ver1.0/assignment3/initial/src/test/CheckSuite.py
@@ -7,16 +7,62 @@ from AST import *
 class CheckSuite(unittest.TestCase):
 
     def test_var_redeclared(self):
-        input = """Let x; Let x;
+        input = """
+        Let x; Let x;
         """
         expect = str(Redeclared(Variable(), "x"))
         self.assertTrue(TestChecker.test(input, expect, 1))
 
     def test_func_redeclared(self):
-        input = """Let x; Function x(){};
+        input = """
+        Let x; Function x(){}
         """
         expect = str(Redeclared(Function(), "x"))
         self.assertTrue(TestChecker.test(input, expect, 2))
+
+    def test_var_and_const(self):
+        input = """
+        Let x; Constant $x;
+        """
+        expect = str(NoEntryPoint())
+        self.assertTrue(TestChecker.test(input, expect, 3))
+
+    def test_const_redeclared(self):
+        input = """Constant $x2; Constant $x2;
+        """
+        expect = str(Redeclared(Constant(), "$x2"))
+        self.assertTrue(TestChecker.test(input, expect, 4))
+
+    def test_2_func_redeclared(self):
+        input = """
+        Function foo(){} 
+        Function foo(){}
+        """
+        expect = str(Redeclared(Function(), "foo"))
+        self.assertTrue(TestChecker.test(input, expect, 5))
+
+    def test_no_entry_point(self):
+        input = """
+        Function foo(){} 
+        """
+        expect = str(NoEntryPoint())
+        self.assertTrue(TestChecker.test(input, expect, 6))
+
+    def test_para_redeclare_1(self):
+        input = """
+        Function foo(b){} 
+        Function main(){} 
+        """
+        expect = ""
+        self.assertTrue(TestChecker.test(input, expect, 7))
+
+    def test_para_redeclare_1(self):
+        input = """
+        Function foo(b, b){} 
+        Function main(){} 
+        """
+        expect = str(Redeclared(Parameter(), "b"))
+        self.assertTrue(TestChecker.test(input, expect, 8))
 
     # def test_undeclared_function(self):
     #     """Simple program: main"""
